554a52ac7b87a8ee48dfda2c4a46b8c801f8e77e
vshringarpure@horizontalintegration.com <vshringarpure@horizontalintegration.com>
2017-03-01 14:23:37 +0530

    Test Commit to Master

---
+++ Test123/Test123/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test123.Startup))]
+namespace Test123
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
